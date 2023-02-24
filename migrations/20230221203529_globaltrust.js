e16f61604bf25c3372b7b02233f555ac1b475f94
Dimitris Lamprinos <pkakelas@gmail.com>
2023-02-24 14:27:43 -0800

    Add strategies to globaltrust migration

--- migrations/20230221203529_globaltrust.js
+++ migrations/20230221203529_globaltrust.js
@@ -4,20 +4,25 @@
  */
 exports.up = function (knex) {
 	return knex.schema.createTable('globaltrust', (table) => {
-		table.text('pretrust')
-		table.text('localtrust')
-		table.float('alpha')
+		table.integer('strategy_id')
 		table.bigInteger('i')
 		table.float('v')
-		table.index(['pretrust', 'localtrust', 'alpha'], 'globaltrust_pretrust_localtrust_alpha_idx')
-		table.unique(['pretrust', 'localtrust', 'alpha', 'i'], 'globaltrust_pretrust_localtrust_alpha_i_idx')
-	});
-};
+		table.index('strategy_id', 'globaltrust_id_idx')
+		table.unique(['strategy_id', 'i'], 'globaltrust_id_i_idx')
+	})
+		.createTable('strategies', (table) => {
+			table.increments('id')
+			table.text('pretrust')
+			table.text('localtrust')
+			table.float('alpha')
+			table.unique(['pretrust', 'localtrust', 'alpha'], 'strategies_pt_lt_a_idx')
+		});
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-	return knex.schema.dropTable('globaltrust');
+	return knex.schema.dropTable('globaltrust').dropTable('strategies');
 };
