66d7199eb1cc35e8db27f5e3c066a21ff5e0221a
Dimitris Lamprinos <pkakelas@gmail.com>
2023-03-23 19:03:49 +0200

    Hardcode strategies

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -14,42 +14,41 @@ export const generateStrategies = async () => {
 	// let localStrategies = Object.keys(ltStrategies)
 	// const ALPHAS = [0.5, 0.8]
 
-	// for (const pt of pretrustStrategies) {
-	// 	for (const lt of localStrategies) {
-	// 		for (const alpha of ALPHAS) {
-	// 			await db('strategies')
-	// 				.insert({ pretrust: pt, localtrust: lt, alpha })
-	// 				.onConflict(['pretrust', 'localtrust', 'alpha'])
-	// 				.ignore()
-	// 		}
-	// 	}
-	// }
+	
 	// 
 	// const strategies = await db('strategies')
 
 	// Let's focus on a fixed set of strategies for now
 	const strategies = [
 		{
+			id: 1,
 			pretrust: 'pretrustAllEqually',
 			localtrust: 'existingConnections',
 			alpha: 0.5
 		}, {
+			id: 2,
 			pretrust: 'pretrustAllEqually',
 			localtrust: 'c5m8enhancedConnections',
 			alpha: 0.5
 	  	}, {
+			id: 3,
 			pretrust: 'pretrustOGs',
 			localtrust: 'c5m8enhancedConnections',
 			alpha: 0.5
 	  	}, {
+			id: 4,
 			pretrust: 'pretrustAllEqually',
 			localtrust: 'c5m8col12enhancedConnections',
 			alpha: 0.5,
 	  	}, { 
+			id: 5,
 			pretrust: 'pretrustOGs',
 			localtrust: 'c5m8col12enhancedConnections',
 			alpha: 0.5
 		}
 	]
+
+
+
 	return strategies
 }
