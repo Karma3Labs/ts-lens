081510bcc83ca5b4f9ff8535068438008759b55c
Dimitris Lamprinos <pkakelas@gmail.com>
2023-04-05 17:37:39 +0300

    Change localtrust to calculate collect edges without price

--- recommender/strategies/localtrust.ts
+++ recommender/strategies/localtrust.ts
@@ -4,32 +4,43 @@ import { getDB } from '../../utils';
 export type LocaltrustStrategy = () => Promise<LocalTrust>
 const db = getDB()
 
+type LocaltrustPrams = {
+	followsWeight?: number,
+	commentsWeight?: number,
+	mirrorsWeight?: number,
+	collectsWeight?: number,
+	collectPriceWeight?: number
+}
+
 /**
  * Generates basic localtrust by transforming all existing connections
 */
 
 const getFollows = async () => {
+	const  { totalCount } = await db('follows').count('profile_id as totalCount').first()
 	const follows = await db('follows')
-	.select('profile_id as following_id', 'profiles.id as follower_id')
-	.innerJoin('profiles', 'owner_address', 'follower_address')
+		.select('profile_id as following_id', 'profiles.id as follower_id')
+		.innerJoin('profiles', 'owner_address', 'follower_address')
 
 	let followsMap: any = {}
 	for (const { followerId, followingId } of follows) {
 		followsMap[followerId] = followsMap[followerId] || []
-		followsMap[followerId][followingId] = 1
+		followsMap[followerId][followingId] = 1 / totalCount
 	}
+
 	return followsMap
 }
 
 const getCommentCounts = async () => {
+	const { totalCount } = await db('comments').count('profile_id as totalCount').first()
 	const comments = await db('comments')
 		.select('profile_id', 'to_profile_id', db.raw('count(1) as count'))
 		.groupBy('profile_id', 'to_profile_id')
-
+	
 	let commentsMap: any = {}
 	for (const { profileId, toProfileId, count } of comments) {
 		commentsMap[profileId] = commentsMap[profileId] || {}
-		commentsMap[profileId][toProfileId] = +count
+		commentsMap[profileId][toProfileId] = +count / totalCount
 	}
 
 	console.log('length of comments', comments.length)
@@ -37,6 +48,7 @@ const getCommentCounts = async () => {
 }
 
 const getMirrorCounts = async () => {
+	const { totalCount } = await db('mirrors').count('profile_id as totalCount').first()
 	const mirrors = await db('mirrors')
 		.select('profile_id', 'to_profile_id', db.raw('count(1) as count'))
 		.groupBy('profile_id', 'to_profile_id')
@@ -44,7 +56,7 @@ const getMirrorCounts = async () => {
 	let mirrorsMap: any = {}
 	for (const { profileId, toProfileId, count } of mirrors) {
 		mirrorsMap[profileId] = mirrorsMap[profileId] || {}
-		mirrorsMap[profileId][toProfileId] = +count
+		mirrorsMap[profileId][toProfileId] = +count / totalCount
 	}
 
 	console.log('length of mirrors', mirrors.length)
@@ -52,6 +64,7 @@ const getMirrorCounts = async () => {
 }
 
 const getCollectCounts = async () => {
+	const { totalCount } = await db('collects').count('profile_id as totalCount').first()
 	const collects = await db('collects')
 		.select('profile_id as to_profile_id', 'profiles.id as from_profile_id', db.raw('count(1) as count'))
 		.innerJoin('profiles', 'collector_address', 'owner_address')
@@ -60,7 +73,7 @@ const getCollectCounts = async () => {
 	let collectsMap: any = {}
 	for (const { fromProfileId, toProfileId, count } of collects) {
 		collectsMap[fromProfileId] = collectsMap[fromProfileId] || {}
-		collectsMap[fromProfileId][toProfileId] = +count
+		collectsMap[fromProfileId][toProfileId] = +count / totalCount
 	}
 
 	console.log('length of collects', collects.length)
@@ -71,34 +84,34 @@ const getCollectPrices = async () => {
 	const collects = await db('collects')
 		.select('profile_id as to_profile_id', 'profiles.id as from_profile_id', db.raw('sum(price) as price'))
 		.innerJoin('profiles', 'collector_address', 'owner_address')
-		.where('price', '<>', 0)
 		.groupBy('profile_id', 'profiles.id')
 
-	const { min, max } = await db.with('coll', (q: any) => q.from('collects') 
-		.select('profile_id as to_profile_id', 'profiles.id as from_profile_id', db.raw('sum(price) as price'))
-		.innerJoin('profiles', 'collector_address', 'owner_address')
-		.where('price', '<>', 0)
-		.groupBy('profile_id', 'profiles.id')
-	)
-	.select(db.raw('min(price) as min'), db.raw('max(price) as max')).from('coll').first()
-
+	const { min, max, avg } = await db.with('coll', (q: any) => q.from('collects')
+			.select('profile_id as to_profile_id', 'profiles.id as from_profile_id', db.raw('sum(price) as price'))
+			.innerJoin('profiles', 'collector_address', 'owner_address')
+			.groupBy('profile_id', 'profiles.id')
+			.where('price', '>', 0)
+		)
+		.select(db.raw('min(price) as min'), db.raw('max(price) as max'), db.raw('avg(price) as avg'))
+		.from('coll').first()
+	
 	let collectsMap: any = {}
 
 	for (const { fromProfileId, toProfileId, price } of collects) {
 		collectsMap[fromProfileId] = collectsMap[fromProfileId] || {}
-		// Normalize results from 0 to 10
-		collectsMap[fromProfileId][toProfileId] = ((price - min) / (max - min)) * 9 + 1
+		collectsMap[fromProfileId][toProfileId] = +price ? ((+price - +min) / (+max - +min)) : +avg
 	}
 
 	console.log('length of collects', collects.length)
 	return collectsMap
 }
 
-const getLocaltrust = async (followsWeight: number, commentsWeight: number, mirrorsWeight: number, collectsWeight: number, withPrice = true) => {
-	const collectsMap = collectsWeight > 0 ? (withPrice ? await  getCollectPrices() : await getCollectCounts()) : null
-	const follows = followsWeight > 0 ? await getFollows() : null
-	const commentsMap = commentsWeight > 0 ? await getCommentCounts() : null
-	const mirrorsMap = mirrorsWeight > 0 ? await getMirrorCounts() : null
+const getLocaltrust = async ({followsWeight, commentsWeight, mirrorsWeight, collectsWeight, collectPriceWeight }: LocaltrustPrams): Promise<LocalTrust> => {
+	const follows = followsWeight ? await getFollows() : null
+	const commentsMap = commentsWeight ? await getCommentCounts() : null
+	const mirrorsMap = mirrorsWeight ? await getMirrorCounts() : null
+	const collectsMap = collectsWeight ? await getCollectCounts() : null
+	const collectsPriceMap = collectPriceWeight ? await getCollectPrices() : null
 
 	let localtrust: LocalTrust = []
 
@@ -122,14 +135,16 @@ const getLocaltrust = async (followsWeight: number, commentsWeight: number, mirr
 			const commentsCount = commentsMap && commentsMap[+id1] && commentsMap[+id1][+id2] || 0
 			const mirrorsCount = mirrorsMap && mirrorsMap[+id1] && mirrorsMap[+id1][+id2] || 0
 			const collectsCount = collectsMap && collectsMap[+id1] && collectsMap[+id1][+id2] || 0
+			const collectPrice = collectsPriceMap && collectsPriceMap[+id1] && collectsPriceMap[+id1][+id2] || 0
 			
 			localtrust.push({
 				i: +id1,
 				j: +id2,
-				v: commentsWeight * commentsCount +
-				mirrorsWeight * mirrorsCount +
-				collectsWeight * collectsCount +
-				followsWeight * follow
+				v: (followsWeight || 0) * follow +
+				(commentsWeight || 0) * commentsCount +
+				(mirrorsWeight || 0) * mirrorsCount + 
+				(collectsWeight || 0) * collectsCount +
+				(collectPriceWeight || 0) * collectPrice
 			})
 		}
 	}
@@ -141,19 +156,19 @@ const getLocaltrust = async (followsWeight: number, commentsWeight: number, mirr
 }
 
 const existingConnections: LocaltrustStrategy = async (): Promise<LocalTrust> => {
-	return getLocaltrust(1, 0, 0, 0)
+	return getLocaltrust({ followsWeight: 1 })
 }
 
 const f6c3m8enhancedConnections: LocaltrustStrategy = async (): Promise<LocalTrust> => {
-	return getLocaltrust(6, 3, 8, 0)
+	return getLocaltrust({ followsWeight: 6, commentsWeight: 3, mirrorsWeight: 8 })
 }
 
 const f6c3m8col12enhancedConnections: LocaltrustStrategy = async (): Promise<LocalTrust> => {
-	return getLocaltrust(6, 3, 8, 12)
+	return getLocaltrust({ followsWeight: 6, commentsWeight: 3, mirrorsWeight: 8, collectsWeight: 12 })
 }
 
 const f6c3m8col12PriceEnhancedConnections: LocaltrustStrategy = async (): Promise<LocalTrust> => {
-	return getLocaltrust(6, 3, 8, 12, true)
+	return getLocaltrust({ followsWeight: 6, commentsWeight: 3, mirrorsWeight: 8, collectPriceWeight: 12 })
 }
 
 export const strategies: Record<string, LocaltrustStrategy> = {
