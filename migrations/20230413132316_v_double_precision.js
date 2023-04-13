14aa2e29211966b5f6b08ee5935383a13b03419f
Dimitris Lamprinos <pkakelas@gmail.com>
2023-04-13 16:24:05 +0300

    Make v double precision in migration

---
+++ migrations/20230413132316_v_double_precision.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.raw('alter table globaltrust alter column v type double precision')
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.raw('alter table globaltrust alter column v type numeric')
+};
