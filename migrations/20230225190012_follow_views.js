82f2c0a28223a880f81ba57980402402fbbdd819
jacksteroo <1073117+jacksteroo@users.noreply.github.com>
2023-06-13 16:18:34 -0700

    Updating materialized view for follower count to match Lens API

--- migrations/20230225190012_follow_views.js
+++ migrations/20230225190012_follow_views.js
@@ -6,12 +6,12 @@ exports.up = function (knex) {
 	return knex.schema.raw(`
 		create materialized view if not exists k3l_follow_counts as (
 			SELECT
-				to_profile_id,
+				follower.follow_profile_id AS to_profile_id,
 				count(*) AS count
-			FROM
-				k3l_follows
-			GROUP BY
-				to_profile_id
+		    FROM
+				follower
+		    GROUP BY
+				follower.follow_profile_id
 		);
 		create index k3l_follow_counts_profile_id_idx on k3l_follow_counts(to_profile_id);
 
