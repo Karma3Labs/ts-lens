b28d53112293d5dfeffd329ac65d15c4ba2f330c
Vijay Mariadassou <vijay@karma3labs.com>
2023-06-15 17:59:39 -0700

    changed sql queries to avoid same handle from showing up multiple times

--- recommender/strategies/feed.ts
+++ recommender/strategies/feed.ts
@@ -7,55 +7,69 @@ const db = getDB()
 
 export const viralFeedWithEngagement = async (limit: number) => {
 	const res = await db.raw(`
-		WITH max_values AS (
+		WITH posts_with_stats AS (	
 			SELECT
-					MAX(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - created_at))/(60*60*24))::integer AS max_age_days,
-					MAX(total_amount_of_mirrors) AS max_mirrors_count,
-					MAX(total_amount_of_comments) AS max_comments_count,
-					MAX(total_amount_of_collects) AS max_collects_count
-			FROM
-					publication_stats
-				INNER JOIN
-					k3l_posts as k3l
-					ON publication_id = k3l.post_id
-				INNER JOIN 
-					profile_post as post
-					ON publication_id = post.post_id
-			WHERE 
-				k3l.created_at > now() - interval '14 days'
-				AND post.is_related_to_post IS NULL
-				AND post.is_related_to_comment IS NULL
-		),
-		posts_with_stats AS (
-				SELECT
-						p.post_id,
-						p.content_uri,
-						p.profile_id,
-						p.created_at,
-						ps.total_amount_of_mirrors AS mirrors_count,
-						ps.total_amount_of_comments AS comments_count,
-						ps.total_amount_of_collects AS collects_count,
-						(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - p.created_at)) / (60 * 60 * 24))::integer AS age_days,
-						gt.v AS globaltrust_v
-				FROM
-						k3l_posts p
-				INNER JOIN publication_stats ps 
-					ON (ps.publication_id = p.post_id AND 
-								(ps.total_amount_of_mirrors + ps.total_amount_of_comments + ps.total_amount_of_collects) > 0)
-				INNER JOIN profile_post post ON post.post_id = p.post_id
-				INNER JOIN globaltrust gt ON gt.i = p.profile_id
-				WHERE
-					gt.strategy_name = 'engagement'
-				AND
-					gt.date = (select max(date) from globaltrust)
-				AND
-					p.created_at > now() - interval '14 days'
-				AND
-					post.is_related_to_post IS NULL
-				AND 
-					post.is_related_to_comment IS NULL
-		)
+				*,
+				ROW_NUMBER() OVER (PARTITION BY profile_id ORDER BY v DESC) AS r_num
+				FROM (
+					SELECT 
+							p.post_id,
+							p.content_uri,
+							p.profile_id,
+							p.created_at,
+							ps.total_amount_of_mirrors AS mirrors_count,
+							ps.total_amount_of_comments AS comments_count,
+							ps.total_amount_of_collects AS collects_count,
+							(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - p.created_at)) / (60 * 60 * 24))::integer AS age_days,
+							(
+									2 * (ps.total_amount_of_comments::numeric / max_values.max_comments_count) +
+									5 * (ps.total_amount_of_mirrors::numeric / max_values.max_mirrors_count) +
+									3 * (ps.total_amount_of_collects::numeric / max_values.max_collects_count) +
+									10 * gt.v -
+									2 * ((EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - p.created_at)) / (60 * 60 * 24))::integer
+																														/ max_values.max_age_days)
+							) AS v,
+							gt.v AS globaltrust_v	
+					FROM
+							(
+								SELECT
+										MAX(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - k3l.created_at))/(60*60*24))::integer AS max_age_days,
+										MAX(total_amount_of_mirrors) AS max_mirrors_count,
+										MAX(total_amount_of_comments) AS max_comments_count,
+										MAX(total_amount_of_collects) AS max_collects_count
+								FROM
+										publication_stats
+									INNER JOIN
+										k3l_posts AS k3l
+										ON publication_id = k3l.post_id
+									INNER JOIN 
+										profile_post AS post
+										ON publication_id = post.post_id
+								WHERE 
+									k3l.created_at > now() - interval '14 days'
+									AND post.is_related_to_post IS NULL
+									AND post.is_related_to_comment IS NULL
+						) max_values,
+							k3l_posts p
+					INNER JOIN publication_stats ps 
+						ON (ps.publication_id = p.post_id AND 
+									(ps.total_amount_of_mirrors + ps.total_amount_of_comments + ps.total_amount_of_collects) > 0)
+					INNER JOIN profile_post post ON post.post_id = p.post_id
+					INNER JOIN globaltrust gt ON gt.i = p.profile_id
+					WHERE
+						gt.strategy_name = 'engagement'
+					AND
+						gt.date = (select max(date) from globaltrust)
+					AND
+						p.created_at > now() - interval '14 days'
+					AND
+						post.is_related_to_post IS NULL
+					AND 
+						post.is_related_to_comment IS NULL
+					ORDER BY p.profile_id, v DESC
+				) AS pstats)
 		SELECT
+				r_num,
 				post_id,
 				content_uri,
 				profile_id,
@@ -63,21 +77,11 @@ export const viralFeedWithEngagement = async (limit: number) => {
 				mirrors_count,
 				comments_count,
 				collects_count,
-				2 * (comments_count::numeric / max_values.max_comments_count) as comments_weight,
-				5 * (mirrors_count::numeric / max_values.max_mirrors_count) as mirrors_weight,
-				3 * (collects_count::numeric / max_values.max_collects_count) as collects_weight,
-				10 * globaltrust_v as globaltrust_weight,
-				2 * (age_days::numeric / max_values.max_age_days) as days_weight,
 				age_days,
-				(
-						2 * (comments_count::numeric / max_values.max_comments_count) +
-						5 * (mirrors_count::numeric / max_values.max_mirrors_count) +
-						3 * (collects_count::numeric / max_values.max_collects_count) +
-						10 * globaltrust_v -
-						2 * (age_days::numeric / max_values.max_age_days)
-				) AS v
+				v
 		FROM
-				posts_with_stats, max_values
+			posts_with_stats
+		WHERE r_num = 1
 		ORDER BY
 				v DESC
 		LIMIT :limit;
--- recommender/strategies/personalfeed.ts
+++ recommender/strategies/personalfeed.ts
@@ -7,56 +7,70 @@ const db = getDB()
 
 export const followingViralFeedWithEngagement = async (limit: number, id: string) => {
 	const res = await db.raw(`
-		WITH max_values AS (
+		WITH posts_with_stats AS (
 			SELECT
-					MAX(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - created_at))/(60*60*24))::integer AS max_age_days,
-					MAX(total_amount_of_mirrors) AS max_mirrors_count,
-					MAX(total_amount_of_comments) AS max_comments_count,
-					MAX(total_amount_of_collects) AS max_collects_count
-			FROM
-					publication_stats
-				INNER JOIN
-					k3l_posts as k3l
-					ON publication_id = k3l.post_id
-				INNER JOIN 
-					profile_post as post
-					ON publication_id = post.post_id
-			WHERE 
-				k3l.created_at > now() - interval '14 days'
-				AND post.is_related_to_post IS NULL
-				AND post.is_related_to_comment IS NULL
-		),
-		posts_with_stats AS (
-				SELECT
-						p.post_id,
-						p.content_uri,
-						p.profile_id,
-						p.created_at,
-      			prof.handle,
-						ps.total_amount_of_mirrors AS mirrors_count,
-						ps.total_amount_of_comments AS comments_count,
-						ps.total_amount_of_collects AS collects_count,
-      			ps.total_upvotes AS upvotes_count,
-						(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - p.created_at)) / (60 * 60 * 24))::integer AS age_days,
-						gt.v AS globaltrust_v
-				FROM
-						k3l_posts p
-				INNER JOIN publication_stats ps 
-					ON (ps.publication_id = p.post_id AND 
-								(ps.total_amount_of_mirrors + ps.total_amount_of_comments + ps.total_amount_of_collects) > 0)
-				INNER JOIN profile_post post ON post.post_id = p.post_id
-				INNER JOIN globaltrust gt ON gt.i = p.profile_id
-      	INNER JOIN k3l_profiles prof ON prof.profile_id = p.profile_id
-				WHERE
-					gt.strategy_name = 'engagement'
-				AND
-					gt.date = (select max(date) from globaltrust)
-				AND
-					p.created_at > now() - interval '14 days'
-				AND
-					post.is_related_to_post IS NULL
-				AND 
-					post.is_related_to_comment IS NULL
+				*,
+				ROW_NUMBER() OVER (partition by profile_id ORDER BY v desc) AS r_num
+				FROM (
+					SELECT	
+							p.post_id,
+							p.content_uri,
+							p.profile_id,
+							p.created_at,
+							prof.handle,
+							ps.total_amount_of_mirrors AS mirrors_count,
+							ps.total_amount_of_comments AS comments_count,
+							ps.total_amount_of_collects AS collects_count,
+							ps.total_upvotes AS upvotes_count,
+							(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - p.created_at)) / (60 * 60 * 24))::integer AS age_days,
+							(
+								2 * (ps.total_amount_of_comments::numeric / max_values.max_comments_count) +
+								5 * (ps.total_amount_of_mirrors::numeric / max_values.max_mirrors_count) +
+								3 * (ps.total_amount_of_collects::numeric / max_values.max_collects_count) +
+								10 * gt.v -
+								2 * ((EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - p.created_at)) / (60 * 60 * 24))::numeric
+																												/ max_values.max_age_days)
+							) AS v,
+							gt.v AS globaltrust_v
+					FROM
+							(
+								SELECT
+										MAX(EXTRACT(EPOCH FROM (CURRENT_TIMESTAMP - created_at))/(60*60*24))::integer AS max_age_days,
+										MAX(total_amount_of_mirrors) AS max_mirrors_count,
+										MAX(total_amount_of_comments) AS max_comments_count,
+										MAX(total_amount_of_collects) AS max_collects_count
+								FROM
+										publication_stats
+									INNER JOIN
+										k3l_posts as k3l
+										ON publication_id = k3l.post_id
+									INNER JOIN 
+										profile_post as post
+										ON publication_id = post.post_id
+								WHERE 
+									k3l.created_at > now() - interval '14 days'
+									AND post.is_related_to_post IS NULL
+									AND post.is_related_to_comment IS NULL
+							) max_values,
+							k3l_posts p
+					INNER JOIN publication_stats ps 
+						ON (ps.publication_id = p.post_id AND 
+									(ps.total_amount_of_mirrors + ps.total_amount_of_comments + ps.total_amount_of_collects) > 0)
+					INNER JOIN profile_post post ON post.post_id = p.post_id
+					INNER JOIN globaltrust gt ON gt.i = p.profile_id
+					INNER JOIN k3l_profiles prof ON prof.profile_id = p.profile_id
+					WHERE
+						gt.strategy_name = 'engagement'
+					AND
+						gt.date = (select max(date) from globaltrust)
+					AND
+						p.created_at > now() - interval '14 days'
+					AND
+						post.is_related_to_post IS NULL
+					AND 
+						post.is_related_to_comment IS NULL
+					ORDER BY p.profile_id, v DESC
+			) AS pstats
 		)
 		SELECT
 				CASE 
@@ -70,22 +84,16 @@ export const followingViralFeedWithEngagement = async (limit: number, id: string
 				mirrors_count,
 				comments_count,
 				collects_count,
-        upvotes_count,
-				(
-						2 * (comments_count::numeric / max_values.max_comments_count) +
-						5 * (mirrors_count::numeric / max_values.max_mirrors_count) +
-						3 * (collects_count::numeric / max_values.max_collects_count) +
-						10 * globaltrust_v -
-						2 * (age_days::numeric / max_values.max_age_days)
-				) AS v
+				upvotes_count,
+				v
 		FROM
 				posts_with_stats AS stats
-        CROSS JOIN max_values
-      	LEFT OUTER JOIN
-      		k3l_follows AS follows
-      		ON (stats.profile_id = follows.to_profile_id AND follows.profile_id = :id)
+				LEFT OUTER JOIN
+					k3l_follows AS follows
+					ON (stats.profile_id = follows.to_profile_id AND follows.profile_id = :id)
+		WHERE r_num = 1
 		ORDER BY
-			following_post DESC,v DESC
+			following_post DESC, v DESC
 		LIMIT :limit;
 	`, { limit, id })
 
