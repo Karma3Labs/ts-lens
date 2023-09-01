f33fdf6171d0dc42afd17005fe478d7dfc08901a
Vijay Mariadassou <vijay@karma3labs.com>
2023-08-31 17:01:07 -0700

    fixed build error due to missing schema

--- scripts/getGlobalTrust.ts
+++ scripts/getGlobalTrust.ts
@@ -35,6 +35,7 @@ const main = async () => {
 	const globaltrust = await RankingsRecommender.calculateByStrategy(
 		await getIds(),
 		{ name: 'test', pretrust: argv.pretrust, localtrust: argv.localtrust, alpha: argv.alpha },
+		'public',
 		false
 	)
  	//recommender.globaltrust = [{i: 1, v: 2}, {i: 3, v: 1}, {i: 2, v: 0.5}]
